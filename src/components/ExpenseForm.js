d82345c7a3f549deef103c132d9b41755d2550f9
Santhosh Kumar Sellavel <skrece22@gmail.com>
2017-12-12 13:00:38 +0530

    Setup formatting for amount and createdAt

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -54,7 +54,7 @@ export default class ExpenseForm extends React.Component {
         this.setState(() => ({error:""}));
         this.props.onSubmit({
             description: this.state.description,
-            amount: parseFloat(this.state.amount, 0)*100,
+            amount: parseFloat(this.state.amount, 10)*100,
             createdAt: this.state.createdAt.valueOf(), 
             note: this.state.note
         });
